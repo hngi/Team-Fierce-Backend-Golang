e852ac7931f275aad03769136d46458285131238
Faithfulness Alamu <vaguemail369@gmail.com>
2020-06-11 17:46:03 +0100

    Fix typo

--- model/structure.go
+++ model/structure.go
@@ -1,19 +1,18 @@
 package model
 
-
-//MailerService is an imterface 
-type MailerService interface{
+//MailerService is an imterface
+type MailerService interface {
 	Send()
 	SendWithTemplate()
 	SendMultiple()
 }
 
 //Mailer struct
-type Mailer struct{
+type Mailer struct {
 	client MailerService
 }
 
 //NewMailer function
-func NewMailer(client Mailerservice) *Mailer{
-	
+func NewMailer(client MailerService) *Mailer {
+
 }
