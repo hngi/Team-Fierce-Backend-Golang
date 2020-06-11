f8f929c6505c1e7c8501bfaa2f2f667dde34099d
Faithfulness Alamu <vaguemail369@gmail.com>
2020-06-11 18:23:17 +0100

    Add logging to new mailer service dispenser

--- model/structure.go
+++ model/structure.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"fmt"
+
 	"github.com/hngi/Team-Fierce.Backend-Golang/mailgun"
 	"github.com/hngi/Team-Fierce.Backend-Golang/sendgrid"
 )
@@ -23,10 +25,13 @@ type MailerService interface {
 func NewMailerService(transport string) *MailerService {
 	switch transport {
 	case tpSendgrid:
+		fmt.Printf("For transport %s, returning a Sendgrid mailer service", transport)
 		return sendgrid.New()
 	case tpMailgun:
+		fmt.Println("For transport %s, returning a Mailgun mailer service", transport)
 		return mailgun.New()
 	default:
+		fmt.Printf("Transport %s not recognized\n", transport)
 		return nil
 	}
 }
